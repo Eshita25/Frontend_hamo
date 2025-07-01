43b0eee605077560ee0c52318abce0a5e144d6e7
Eshita25 <121708189+Eshita25@users.noreply.github.com>
2025-07-01 12:13:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,8 @@ import {getDatabase} from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRo2lU-0Pi-Ze6pQvgglxbUZsuT05IRJU",
-  authDomain: "hamo102.firebaseapp.com",
-  databaseURL: "https://hamo102-default-rtdb.firebaseio.com",
-  projectId: "hamo102",
-  storageBucket: "hamo102.firebasestorage.app",
-  messagingSenderId: "669360629660",
-  appId: "1:669360629660:web:abba625473691b3a1bca59",
-  measurementId: "G-CK9ERCWREY"
+
+
 };
 
 // Initialize Firebase
